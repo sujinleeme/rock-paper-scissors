e582b549f27e11e96cc529c538f0be6f31c430cc
Sujin Lee <6011401+sujinleeme@users.noreply.github.com>
2022-03-14 18:19:02 +0100

    chore: add @context in moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/styles/__mocks__/styleMock.js',
     '^@types(.*)$': '<rootDir>/@types$1',
+    '^@context(.*)$': '<rootDir>/context$1',
     '^@components(.*)$': '<rootDir>/components$1',
     '^@pages(.*)$': '<rootDir>/pages$1',
     '^@hooks(.*)$': '<rootDir>/hooks$1',
