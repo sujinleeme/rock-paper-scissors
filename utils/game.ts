f384c30668e4925c7f4fa9ea1020c9b333319895
Sujin Lee <6011401+sujinleeme@users.noreply.github.com>
2022-03-14 14:41:07 +0100

    feat: create getWinner and update test

---
+++ utils/game.ts
@@ -0,0 +1,12 @@
+import { Choice, Winner } from '../@types'
+
+type GetWinner = (p1: Choice, p2: Choice) => Winner
+
+export const getWinner: GetWinner = (p1, p2) => {
+  // The player2 wins if move is one grater than player1
+  if ((p1 + 1) % 3 === p2) return Winner.p2
+  // It draws because both players have the same move
+  if (p1 === p2) return Winner.draw
+  // The player1 wins because it's not a draw and that player 2 didn't win
+  return Winner.p1
+}
