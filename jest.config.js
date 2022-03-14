a49f2a77c1798b0b8e9a1f492677d00e0a62760e
Sujin Lee <6011401+sujinleeme@users.noreply.github.com>
2022-03-14 14:39:07 +0100

    feat: setup jest, testing-library

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/styles/__mocks__/styleMock.js',
+    '^@types(.*)$': '<rootDir>/@types$1',
+    '^@components(.*)$': '<rootDir>/components$1',
+    '^@pages(.*)$': '<rootDir>/pages$1',
+    '^@hooks(.*)$': '<rootDir>/hooks$1',
+  },
+}
