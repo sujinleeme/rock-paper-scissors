af5848146a7a6d1147731846fdd97d77d9c5d439
Sujin Lee <6011401+sujinleeme@users.noreply.github.com>
2022-05-18 19:29:52 +0200

    fix: remove global hover of button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -9,18 +9,15 @@ export interface ButtonProps extends ButtonPropsBase {
 export const Button: React.FC<ButtonProps> = ({
   variant = 'secondary',
   ...props
-}) => {
-  return (
-    <ButtonBase
-      boxShadow="md"
-      rounded="full"
-      fontWeight="normal"
-      outline={1}
-      variant={variant === 'primary' ? 'solid' : 'outline'}
-      _hover={{ bg: '#ebedf0' }}
-      px={6}
-      py={5}
-      {...props}
-    />
-  )
-}
+}) => (
+  <ButtonBase
+    boxShadow="md"
+    rounded="full"
+    fontWeight="normal"
+    outline={1}
+    variant={variant === 'primary' ? 'solid' : 'outline'}
+    px={6}
+    py={5}
+    {...props}
+  />
+)
